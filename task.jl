5cf3e102912bd63d5d29895cd518c3a48f168314
tianchi02 <47800377+tianchi02@users.noreply.github.com>
2020-01-17 23:01:34 +0800

    Update task.jl

--- task.jl
+++ task.jl
@@ -1,17 +1,6 @@
 
-"""
-print `param` (take **String** type as the argument)
 
-# print the argument once it is passed inside the function
-
-```julia
-julia> str = "Hello World\n"
-julia> Print(str*str)
-$(let str = "Hello World"
-    str*str
-end)
-```
-"""
-function Print(param::String)
-    println(param)
-    end
+function multiplier1(x,y)
+""" returns the product of  input x and y """
+  return x*y
+end
